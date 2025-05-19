a1ab2578b465385913e1f733b5160bea5949ddfc
Yves Trang <trang.yveschri@gmail.com>
2025-05-19 15:48:03 +0200

    ajout d'une databasa avec les exo et un memory_state

--- app.py
+++ app.py
@@ -1,64 +1,64 @@
-import io
+
 
 import duckdb
-import pandas as pd
+
 import streamlit as st
 
-csv = """
-beverage,price
-Orange juice,2.5
-Expresso,2
-Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(csv))
+#answer = """
+#"SELECT * FROM beverages
+#CROSS JOIN food_items
+#"""
 
-csv2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-food_items = pd.read_csv(io.StringIO(csv2))
+#solution_df = duckdb.sql(answer).df()
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-answer = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
 
-solution_df = duckdb.sql(answer).df()
+with st.sidebar:
+    theme = st.selectbox(
+        "What would you like to review?",
+        ("cross_joins", "Groupby", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme..."
+    )
+    st.write("You selected:", theme)
+
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercise)
+
+
 st.header("enter your code")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-    if len(result.columns) != len(solution_df.columns):
-        st.write("Some columns are missing")
-
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("some columns are missing")
-
-        n_lines_difference = result.shape[0] - solution_df.shape[0]
-        if n_lines_difference != 0:
-            st.write(
-                f"result has a {n_lines_difference} lines difference with the solution_df"
-            )
-        st.dataframe(result.compare(solution_df))
-
-tab2, tab3 = st.tabs(["Tables", "Solution_df"])
-
-
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution_df)
-
-with tab3:
-    st.write(answer)
+#if query:
+#    result = duckdb.sql(query).df()
+#    st.dataframe(result)
+#
+#    if len(result.columns) != len(solution_df.columns):
+#        st.write("Some columns are missing")
+#
+#    try:
+#        result = result[solution_df.columns]
+#        st.dataframe(result.compare(solution_df))
+#    except KeyError as e:
+#        st.write("some columns are missing")
+#
+#        n_lines_difference = result.shape[0] - solution_df.shape[0]
+#        if n_lines_difference != 0:
+#            st.write(
+#                f"result has a {n_lines_difference} lines difference with the solution_df"
+#            )
+#        st.dataframe(result.compare(solution_df))
+#
+#tab2, tab3 = st.tabs(["Tables", "Solution_df"])
+#
+#
+#with tab2:
+#    st.write("table: beverages")
+#    st.dataframe(beverages)
+#    st.write("table: food_items")
+#    st.dataframe(food_items)
+#    st.write("expected:")
+#    st.dataframe(solution_df)
+#
+#with tab3:
+#    st.write(answer)
---
+++ init_db.py
@@ -0,0 +1,32 @@
+import io
+import pandas as pd
+import duckdb
+
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+
+data = {
+    "theme": ["cross_joins","window_functions"],
+    "exercice_name": ["beverages_and_food","simple_window"],
+    "tables": [["beverages", "food_items"],"simple_window"],
+    "last_reviewed": ["1970-01-01","1970-01-01"]
+}
+memory_state_df = pd.DataFrame(data)
+con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+
+csv = """
+beverage,price
+Orange juice,2.5
+Expresso,2
+Tea,3
+"""
+beverages = pd.read_csv(io.StringIO(csv))
+con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
+
+csv2 = """
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+"""
+food_items = pd.read_csv(io.StringIO(csv2))
+con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
